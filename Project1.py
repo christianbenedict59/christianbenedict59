45d5f651c96f8b350b5c03b1db8529a3771b0de0
christianbenedict59 <158599874+christianbenedict59@users.noreply.github.com>
2024-05-06 20:59:06 -0600

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -11,17 +11,19 @@ df = pd.read_csv("https://github.com/byuidatascience/data4names/raw/master/data-
 #%%
 chart = px.line(df.query("name == 'Robert'"), x="year", y="Total", title="Robert Graph")
 chart.show()
-
+#The chart demonstrates the changes over time of the name Robert, which historically has been one of the most common names.
 
 #%%
 chart_2 = px.line(df.query("name == 'Brittany'"), x="year", y="Total", title="Brittany Graph")
 chart_2.show()
-
+#The chart shows the history of the name Brittany, which has gained popular very recently as the chart shows
 #%%
 names = ['Robert', 'John', 'Mary', 'Martha']  # Fixed the missing comma
 filtered_df = df.query("name in @names and year >= 1930")
 chart_3 = px.line(filtered_df, x='year', y='Total', color='name', title='Name Popularity Trends')
-chart_3.show()
+chart_3.show() 
+#The chart shows the changes of various names from roughly 100 years ago until today, and the changes that occured for each of them
 #%%
 chart_4 = px.line(df.query("name == 'John'"), x="year", y="Total", title="John Graph")
-chart_4.show()
+chart_4.show() 
+#The chart shows the changes of the name John, one of the most popular names ever historically
